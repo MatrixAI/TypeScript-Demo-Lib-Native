f3f15357d453c7f51980e88c76fe539890298ec3
Roger Qiu <roger.qiu@polyhack.io>
2020-09-30 22:59:07 +1000

    Integrating IFD

--- src/bin/typescript-demo-lib.ts
+++ src/bin/typescript-demo-lib.ts
@@ -2,11 +2,13 @@
 
 import process from 'process';
 import Library from '../lib/Library';
+import { v4 as uuidv4 } from 'uuid';
 
 function main(argv = process.argv): number {
   console.log(argv.slice(2));
   const l = new Library('new library');
   console.log(l.someParam);
+  console.log(uuidv4());
   process.exitCode = 0;
   return process.exitCode;
 }
