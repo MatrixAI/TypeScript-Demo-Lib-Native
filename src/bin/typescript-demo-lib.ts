cc02777e0378deb4037ebc32e3d6bbcf36c75b9a
Emma Casolin <ecasolin@hotmail.com>
2021-07-05 13:00:27 +1000

    rebasing
    added tests foroutput of typescript-demo-lib.ts
    
    moved external import above internal import
    
    moved jest-mock-process to devDependencies
    
    updated package-lock.json with changes

--- src/bin/typescript-demo-lib.ts
+++ src/bin/typescript-demo-lib.ts
@@ -2,13 +2,28 @@
 
 import process from 'process';
 import Library from '../lib/Library';
+import NumPair from '../lib/NumPair';
 import { v4 as uuidv4 } from 'uuid';
 
 function main(argv = process.argv): number {
-  console.log(argv.slice(2));
+  // Print out command-line arguments
+  const argArray = argv.slice(2);
+  const args = argArray.toString();
+  process.stdout.write('[' + args + ']\n');
+
+  // Create a new Library with the value someParam = 'new library'
+  // And print it out
   const l = new Library('new library');
-  console.log(l.someParam);
-  console.log(uuidv4());
+  process.stdout.write(l.someParam + '\n');
+
+  // Generate and print a uuid (universally unique identifier)
+  process.stdout.write(uuidv4() + '\n');
+
+  // Add the first two command-line args and print the result
+  const nums = new NumPair(parseInt(argv[2]), parseInt(argv[3]));
+  const sum = nums.num1 + nums.num2;
+  process.stdout.write(nums.num1 + ' + ' + nums.num2 + ' = ' + sum + '\n');
+
   process.exitCode = 0;
   return process.exitCode;
 }
