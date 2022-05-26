83341b9640cab5bcc16e43d62a34f7187a67f80b
Roger Qiu <roger.qiu@polyhack.io>
2022-05-26 11:54:37 +1000

    WIP: attempt to bring in import/no-cycle rule

--- src/lib/Library.ts
+++ src/lib/Library.ts
@@ -1,8 +1,12 @@
+import NumPair from './NumPair';
+
 class Library {
   someParam: string;
+  num: NumPair;
 
   constructor(someParam = 'Parameter') {
     this.someParam = someParam;
+    this.num = new NumPair();
   }
 }
 
--- src/lib/NumPair.ts
+++ src/lib/NumPair.ts
@@ -1,10 +1,14 @@
+import Library from './Library';
+
 class NumPair {
   num1: number;
   num2: number;
+  lib: Library;
 
   constructor(firstNum = 0, secondNum = 0) {
     this.num1 = firstNum;
     this.num2 = secondNum;
+    this.lib = new Library();
   }
 }
 
