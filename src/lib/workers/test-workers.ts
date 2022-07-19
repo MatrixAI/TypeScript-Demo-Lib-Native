02ed69faa6671e729e7cafd1c3dfa5e51a360f4d
Roger Qiu <roger.qiu@polyhack.io>
2022-07-19 22:10:59 +1000

    fix: experiment with disabling workers

--- src/lib/workers/test-workers.ts
+++ src/lib/workers/test-workers.ts
@@ -1,12 +1,10 @@
-import * as os from 'os';
 import process from 'process';
 import { Pool, spawn, Worker } from 'threads';
 
 async function testWorkers() {
   process.stdout.write('Lets test workers.\n');
-  const coreCount = os.cpus().length;
-  const pool = Pool(() => spawn(new Worker('./worker')), coreCount);
-  for (let i = 0; i < coreCount; i++) {
+  const pool = Pool(() => spawn(new Worker('./worker')), 1);
+  for (let i = 0; i < 1; i++) {
     void pool.queue(async (hellower) => {
       process.stdout.write((await hellower.helloWorld()) + '\n');
     });
