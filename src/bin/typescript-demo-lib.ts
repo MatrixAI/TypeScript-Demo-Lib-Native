de25241f58e9788e851b57d5bd64ebacd1be9a03
Roger Qiu <roger.qiu@polyhack.io>
2022-07-19 21:45:30 +1000

    style: lintfixed

--- src/bin/typescript-demo-lib.ts
+++ src/bin/typescript-demo-lib.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import * as os from 'os';
 import process from 'process';
 import { v4 as uuidv4 } from 'uuid';
 import Library from '../lib/Library';
@@ -25,9 +24,9 @@ async function main(argv = process.argv): Promise<number> {
   // Add the first two command-line args and print the result
   // default to using 0
   let num1 = parseInt(argv[0]);
-  num1 = (isNaN(num1)) ? 0 : num1;
+  num1 = isNaN(num1) ? 0 : num1;
   let num2 = parseInt(argv[1]);
-  num2 = (isNaN(num2)) ? 0 : num2;
+  num2 = isNaN(num2) ? 0 : num2;
   const nums = new NumPair(num1, num2);
   const sum = nums.num1 + nums.num2;
   process.stdout.write(nums.num1 + ' + ' + nums.num2 + ' = ' + sum + '\n');
